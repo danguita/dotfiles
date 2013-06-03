2515b40e8ea3cba71a03999630d404e8df8eb558
David Anguita <david@davidanguita.name>
2013-06-03 10:37:28 +0200

    Add autojump ("j" for the win)

--- zsh/custom/include.zsh
+++ zsh/custom/include.zsh
@@ -1,2 +1,8 @@
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # RVM
-[[ -s "$HOME/.nvm/nvm.sh" ]] && . "$HOME/.nvm/nvm.sh"                # NVM
+# Ruby Version Manager
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+# Node Version Manager
+[[ -s "$HOME/.nvm/nvm.sh" ]] && . "$HOME/.nvm/nvm.sh"
+
+# Autojump
+[[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh ]]
