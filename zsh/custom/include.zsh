14b69891bbb580878be287242f15bc3c99ca1a7c
David Anguita <david@davidanguita.name>
2017-01-25 21:03:25 +0100

    Do not handle Node.js versions on the host machine

--- zsh/custom/include.zsh
+++ zsh/custom/include.zsh
@@ -1,6 +1,3 @@
 # Ruby version management: chruby
 source /usr/local/share/chruby/chruby.sh
 source /usr/local/share/chruby/auto.sh
-
-# Node Version Manager
-[[ -s "$HOME/.nvm/nvm.sh" ]] && . "$HOME/.nvm/nvm.sh"
