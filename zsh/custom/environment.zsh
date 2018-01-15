3878aa775da9ab87fea5de1947508a9ad36494e6
David Anguita <david@davidanguita.name>
2018-01-15 17:22:58 +0100

    Make AWS EC2 environmental variables dependent on filesystem

--- zsh/custom/environment.zsh
+++ zsh/custom/environment.zsh
@@ -19,11 +19,12 @@ export PYTHONPATH=${PYTHONPATH:-"/usr/local/lib/python2.7/site-packages"}
 export GOPATH=${GOPATH:-"$HOME/.go"}
 
 # Amazon EC2 API/AMI tools
-export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
-export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
-export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
-export EC2_AMITOOL_HOME="/usr/local/Library/LinkedKegs/ec2-ami-tools/jars"
-export EC2_URL='https://ec2.eu-west-1.amazonaws.com' # Set default region (eu-west / Ireland)
+[ -d "$HOME/.ec2" ] &&
+  export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)" &&
+  export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)" &&
+  export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars" &&
+  export EC2_AMITOOL_HOME="/usr/local/Library/LinkedKegs/ec2-ami-tools/jars" &&
+  export EC2_URL='https://ec2.eu-west-1.amazonaws.com' # Set default region (eu-west / Ireland)
 
 # Docker
 export DOCKER_TLS_VERIFY="1"
