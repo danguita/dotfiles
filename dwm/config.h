da5f2c0d0bc9fb54249f6b07ee056a7e329df4fe
David Anguita <david@davidanguita.name>
2020-05-03 09:16:39 +0200

    (dwm) Disable rules by window class

--- dwm/config.h
+++ dwm/config.h
@@ -21,14 +21,15 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const Rule rules[] = {
+static const Rule rules[0];
+/* static const Rule rules[] = { */
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 }
-};
+	/* { "Gimp",  NULL,       NULL,       0,            1,           -1 } */
+/* }; */
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
