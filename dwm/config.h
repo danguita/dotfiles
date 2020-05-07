42698809b992b7004896ef1553dd5f9f597fb0b0
David Anguita <david@davidanguita.name>
2020-05-07 16:56:05 +0200

    (dwm) Initialize rules array
    This is the documented way of removing any window class specializations.
    This also gets rid of a compilation warning.

--- dwm/config.h
+++ dwm/config.h
@@ -21,15 +21,10 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const Rule rules[0];
-/* static const Rule rules[] = { */
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
+static Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
-	/* { "Gimp",  NULL,       NULL,       0,            1,           -1 } */
-/* }; */
+	{ NULL,       NULL,       NULL,       0,            False,       -1 },
+};
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
