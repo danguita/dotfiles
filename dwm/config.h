b06419ae8da27008150b2f04c7bbb7150ac48066
David Anguita <david@davidanguita.name>
2020-05-10 17:25:10 +0200

    (dwm) set `urxvt` as terminal command
    For compatibility purposes.

--- dwm/config.h
+++ dwm/config.h
@@ -52,7 +52,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "x-terminal-emulator", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
